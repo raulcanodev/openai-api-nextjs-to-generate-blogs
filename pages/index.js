87d96aa3152348ae47ced76eb51e9a009425359a
Raul M <118123543+pillow3@users.noreply.github.com>
2024-05-20 22:09:58 +0100

    set auth

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
+import Link from "next/link"
+
 export default function Home() {
   return <>
-  <h1>Hello world</h1>
+    <div>
+      <h1>Home page</h1>
+      <div>
+        <Link href='/api/auth/login'>Login</Link>
+      </div>
+    </div>
   </>
 }
